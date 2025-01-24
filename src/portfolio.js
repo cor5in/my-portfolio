17056363339ea54edaee401344a0c8a01a49a07e
cor5in <gun@khu.ac.kr>
2025-01-24 14:24:35 +0900

    Updated

--- src/portfolio.js
+++ src/portfolio.js
@@ -38,18 +38,18 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Geon Kim",
-  title: "Glad to see you, I'm Geon",
+  username: "Corbin Kim",
+  title: "Glad to see you, I'm Corbin",
   subTitle: emoji(
-    "A passionate researcher studying vRAN 🚀, eager to collaborate with diverse individuals and achieve innovation"
+    "A dedicated researcher exploring radio resource management and the AI/ML workflows within the RIC architecture in Open RAN-based networks, striving for innovation and impactful solutions. 🚀"
   ),
   displayGreeting: true
 };
 
 // Social Media Links
 const socialMediaLinks = {
-  github: "https://github.com/41vin",
-  linkedin: "https://www.linkedin.com/in/41vin/",
+  github: "https://github.com/cor5in",
+  linkedin: "https://www.linkedin.com/in/cor5in/",
   gmail: "gun@khu.ac.kr",
   instagram: "https://www.instagram.com/gun.__.kim",
   display: true
@@ -59,11 +59,11 @@ const socialMediaLinks = {
 const skillsSection = {
   title: "What I do",
   subTitle:
-    "Researching next-generation mobile communication, particularly cloud native RAN",
+    "Researching next-generation mobile communication, particularly open RAN",
   skills: [
-    emoji("⚡ Research on AI/ML workflow in Open RAN"),
-    emoji("⚡ Study of cloud native RAN architecture"),
-    emoji("⚡ Research on self-organizing network and zero-touch network")
+    emoji("⚡ Research on AI/ML workflow in open RAN"),
+    emoji("⚡ Study on open RAN specifications"),
+    emoji("⚡ Research on building energy-efficient networks through radio resource management")
   ],
 
   /* Make Sure to include correct Font Awesome Classname to view your icon
@@ -82,10 +82,19 @@ const educationInfo = {
       schoolName: "KyungHee University",
       logo: require("./assets/images/khuLogo.png"),
       subHeader:
+        "Ph.D.Candidate in Electronic and Information Convergence Engineering",
+      duration: "March 2025 - Expected",
+      desc: "Belong to Mobile Communications Lab and participate in the research of next-generation communication, focusing on radio resource management and open RAN-based software and specifications.",
+      descBullets: ["Open RAN", "Radio Resource Management"]
+    },
+    {
+      schoolName: "KyungHee University",
+      logo: require("./assets/images/khuLogo.png"),
+      subHeader:
         "Master of Science in Electronic and Information Convergence Engineering",
-      duration: "March 2023 - ",
+      duration: "March 2023 - February 2025",
       desc: "Belong to Mobile Communications Lab, and participate in the research of next-generation communication",
-      descBullets: ["SON & ZTN", "Open RAN"]
+      descBullets: ["Open RAN", "Mobile Traffic Prediction"]
     },
     {
       schoolName: "KyungHee University",
@@ -112,12 +121,12 @@ const techStack = {
       progressPercentage: "70%"
     },
     {
-      Stack: "Programming",
-      progressPercentage: "60%"
+      Stack: "Radio Resource Management Algorithm",
+      progressPercentage: "80%"
     },
     {
-      Stack: "Optimization",
-      progressPercentage: "60%"
+      Stack: "Optimization Theory",
+      progressPercentage: "70%"
     }
   ],
   displayCodersrank: true // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
@@ -136,6 +145,19 @@ const openSource = {
 const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
+    {
+      role: "Open Source Contributor",
+      company: "O-RAN Software Community: AIMLFW",
+      companylogo: require("./assets/images/O-RAN.jpeg"),
+      date: "March 2024 ~ Present",
+      desc: "Researcher",
+      descBullets: [
+        "AIMLFW: Contributor",
+        "Officially registered a new use case: Traffic Forecasting for Green Network",
+        "Developed and maintained the installation process for AIMLFW",
+        "Improved the Training Manager(TM) module to enhance functionality and performance"
+      ]
+    },
     {
       role: "Software Engineer",
       company: "Aether",
@@ -164,12 +186,86 @@ const workExperiences = {
   ]
 };
 
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "Best Paper Award",
+      subtitle:
+        "Recognized for exceptional research contributions to O-RAN testbed implementation at the KICS symposium",
+      image: require("./assets/images/KICS.png"),
+      imageAlt: "KICS Logo",
+      footerLink: []
+    },
+    {
+      title: "Outstanding Research Award",
+      subtitle:
+        "Awarded for the 5G Green AI Algorithm research project conducted with SKT",
+      image: require("./assets/images/SKT.png"),
+      imageAlt: "AWS Logo",
+      footerLink: []
+    },
+    {
+      title: "Best Paper Award",
+      subtitle:
+        "Honored for excellence in research on traffic prediction at the KICS symposium",
+      image: require("./assets/images/AWS-SAA.webp"),
+      imageAlt: "AWS Logo",
+      footerLink: []
+    },
+    {
+      title: "AWS Solutions Architect Associate",
+      subtitle:
+        " Associate exam validates individuals' abilities to architect and deploy secure and robust applications on AWS technologies, crucial for those in cloud architecture roles.",
+      image: require("./assets/images/AWS-SAA.webp"),
+      imageAlt: "AWS Logo",
+      footerLink: []
+    },
+    {
+      title: "Certified Kubernetes Administrator",
+      subtitle:
+        "A certification offered by the Cloud Native Computing Foundation (CNCF) that validates one's expertise in deploying, managing, and troubleshooting Kubernetes clusters",
+      image: require("./assets/images/CKA.png"),
+      imageAlt: "CKA Logo",
+      footerLink: []
+    },
+    {
+      title: "Data Science: Coaching Study, Leader",
+      subtitle: "A certification offered by the Naver Connect Foundation",
+      image: require("./assets/images/naver.png"),
+      imageAlt: "Naver connect foundation logo",
+      footerLink: []
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+
 // Some big projects you have worked on
 
 const bigProjects = {
   title: "Team Projects",
   subtitle: "PROJECTS ACCOMPLISHED WITH OTHER INDIVIDUALS",
   projects: [
+    {
+      image: require("./assets/images/O-RAN.jpeg"),
+      projectName: "Open Source Contribution Academy",
+      projectDesc: "O-RAN Software Community: AIMLFW",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://lf-o-ran-sc.atlassian.net/wiki/spaces/AIMLFEW/overview?homepageId=13697038"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
     {
       image: require("./assets/images/SKT.png"),
       projectName: "SKT AI Fellowship",
@@ -210,53 +306,29 @@ const bigProjects = {
   display: true // Set false to hide this section, defaults to true
 };
 
-// Achievement Section
-// Include certificates, talks etc
 
-const achievementSection = {
-  title: emoji("Achievements And Certifications 🏆 "),
-  subtitle:
-    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
-
-  achievementsCards: [
-    {
-      title: "AWS Solutions Architect Associate",
-      subtitle:
-        " Associate exam validates individuals' abilities to architect and deploy secure and robust applications on AWS technologies, crucial for those in cloud architecture roles.",
-      image: require("./assets/images/AWS-SAA.webp"),
-      imageAlt: "AWS Logo",
-      footerLink: []
-    },
-    {
-      title: "Certified Kubernetes Administrator",
-      subtitle:
-        "A certification offered by the Cloud Native Computing Foundation (CNCF) that validates one's expertise in deploying, managing, and troubleshooting Kubernetes clusters",
-      image: require("./assets/images/CKA.png"),
-      imageAlt: "CKA Logo",
-      footerLink: []
-    },
-    {
-      title: "Data Science: Coaching Study, Leader",
-      subtitle: "A certification offered by the Naver Connect Foundation",
-      image: require("./assets/images/naver.png"),
-      imageAlt: "Naver connect foundation logo",
-      footerLink: []
-    }
-  ],
-  display: true // Set false to hide this section, defaults to true
-};
 
 // Blogs Section
 
 const blogSection = {
   title: "Blogs",
   subtitle:
-    "Content related to 5G core, Open RAN, and various open-source technologies",
+    "Content focused on research papers and personal records related to open RAN and various research topics",
   blogs: [
     {
-      url: "https://41vin.github.io/docs/2023_08_23_OTIC/",
-      title: "1st OTIC in SUTD, Singapore",
-      description: "My Participation Review of the 1st OTIC"
+      url: "https://cor5in.github.io/blog/paper/2024-09-26-joint-traffic-prediction-and-base-station-sleepiong-for-energy-saving-in-cellular-networks/",
+      title: "[Review]Joint Traffic Prediction and Base Station Sleeping for Energy Saving in Cellular Networks",
+      description: "A paper review conducted as part of preliminary research on energy-saving via traffic prediction and base station sleeping in cellular networks"
+    },
+    {
+      url: "https://cor5in.github.io/blog/paper/2024-11-15-AI-ready-Energy-Modeling-for-Next-Generation-RAN/",
+      title: "[Review]AI-Ready Energy Modelling for Next Generation RAN",
+      description: "A review of the paper referenced for implementing a cell on/off simulator for AI-ready energy modeling in next-generation RAN"
+    },
+    {
+      url: "https://cor5in.github.io/blog/paper/2025-01-13-analyzing-and-modeling-spatil-temporal-dependence-of-cellular-traffic-at-city-scale/",
+      title: "[Review]Analyzing and Modeling Spatio-Temporal Dependence of Cellular Traffic at City Scale",
+      description: "A review of the paper referenced for developing algorithms to analyze SKT’s real base station traffic data at a city scale"
     }
   ],
   display: true // Set false to hide this section, defaults to true
